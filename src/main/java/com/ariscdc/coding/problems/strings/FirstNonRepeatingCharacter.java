110e134775c1cb285df7a66bcce7e815c6488595
Aris Dela Cruz <ardelacruz@mssngsolutions.ph>
2017-09-19 20:22:27 +0800

    Added Maven support. Solved String problems - FirstNonRepeatingCharacter, SwapVowels.

---
+++ src/main/java/com/ariscdc/coding/problems/strings/FirstNonRepeatingCharacter.java
@@ -0,0 +1,75 @@
+package com.ariscdc.coding.problems.strings;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * @author ariscdc
+ *
+ * 20170919 1900-1945 (45 mins.)
+ *                    (~30 mins. solution design)
+ * Time: O(n) - Need to iterate each character in String.
+ * Space: O(n) - Worst case all characters are unique.
+ */
+public class FirstNonRepeatingCharacter {
+
+    private Character findFirstNonRepeatingCharacter(String text) {
+
+        LinkedHashSet<Character> charactersRead = new LinkedHashSet<>();
+        Set<Character> duplicateCharacters = new HashSet<>();
+
+        if (text == null || text.length() == 0) {
+            throw new IllegalArgumentException("Input text is invalid.");
+        }
+
+        for (int i = 0; i < text.length(); i++) {
+            Character ch = text.charAt(i);
+            if (charactersRead.contains(ch)) {
+                charactersRead.remove(ch);
+                duplicateCharacters.add(ch);
+
+            } else if (!duplicateCharacters.contains(ch)){
+                charactersRead.add(ch);
+            }
+        }
+        if (!charactersRead.isEmpty()) {
+            return charactersRead.iterator().next();
+        } else {
+            throw new IllegalArgumentException("No non-repeating character found.");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FirstNonRepeatingCharacter app = new FirstNonRepeatingCharacter();
+
+        System.out.println("abcdab" + " = " + app.findFirstNonRepeatingCharacter("abcdab"));
+        System.out.println("abcdcd" + " = " + app.findFirstNonRepeatingCharacter("abcdcd"));
+        System.out.println("aaab" + " = " + app.findFirstNonRepeatingCharacter("aaab"));
+        System.out.println("abbb" + " = " + app.findFirstNonRepeatingCharacter("abbb"));
+        System.out.println("abcdefghijklmnopqrstuvwxyz" + " = " + app.findFirstNonRepeatingCharacter("abcdefghijklmnopqrstuvwxyz"));
+
+        try {
+            String text = null;
+            System.out.print(text + " = ");
+            app.findFirstNonRepeatingCharacter(text);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            String text = "";
+            System.out.print(text + " = ");
+            app.findFirstNonRepeatingCharacter(text);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            String text = "abab";
+            System.out.print(text + " = ");
+            app.findFirstNonRepeatingCharacter(text);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/ariscdc/coding/problems/strings/SwapVowels.java
@@ -0,0 +1,125 @@
+package com.ariscdc.coding.problems.strings;
+
+/**
+ * @author ariscdc
+ *
+ * 20170909 2025-2044 (19 mins.)
+ * 20170910 0840-0854 (14 mins.)
+ *                    (33 mins.)
+ */
+public class SwapVowels {
+
+    private static boolean isVowel(char ch) {
+        switch (ch) {
+            case 'A':
+            case 'a':
+            case 'E':
+            case 'e':
+            case 'I':
+            case 'i':
+            case 'O':
+            case 'o':
+            case 'U':
+            case 'u':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private static void swap(char[] charArray, int left, int right) {
+        char temp = charArray[left];
+        charArray[left] = charArray[right];
+        charArray[right] = temp;
+    }
+
+    private static String swapVowels(String text) {
+
+        if (text == null) {
+            return null;
+        }
+        char[] charArray = text.toCharArray();
+
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            while (left < charArray.length && !isVowel(charArray[left])) {
+                left++;
+            }
+            while (left < right && !isVowel(charArray[right])) {
+                right--;
+            }
+            if (left < right) {
+                swap(charArray, left++, right--);
+            }
+        }
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(swapVowels("apple"));
+        System.out.println(swapVowels("frost"));
+        System.out.println(swapVowels("aeio"));
+        System.out.println(swapVowels("jklm"));
+        System.out.println(swapVowels("friend"));
+
+        /*
+        Possible Questions on Requirements:
+
+        1. Case sensitive?
+        2. Do I need to swap if the same vowel?
+        3. In-place swapping?
+        ==============================
+
+        Test Case 1 - Both Ends
+
+        apple
+        eppla
+        01234
+
+        left = 0 1 3
+        right = 4 3
+
+        ------------------------------
+        Test Case 2 - Middle
+
+        frost
+        01234
+
+        left = 0 2
+        right = 4 2
+
+        ------------------------------
+        Test Case 3 - All vowels
+
+        aeio
+        oeia
+        oiea
+        0123
+
+        left = 0 1 2
+        right = 3 2 1
+
+        ------------------------------
+        Test Case 4 - No vowels
+
+        jklm
+        0123
+
+        left = 0 1 2 3
+        right = 3
+
+        ------------------------------
+        Test Case 5 - Even middle
+
+        friend
+        freind
+        012345
+
+        left = 0 1 2 3
+        right = 5 4 3 2
+         */
+    }
+}
