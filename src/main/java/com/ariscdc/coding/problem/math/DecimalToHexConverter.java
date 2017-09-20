2560304ca1dfeff0f039ad7607cc8107282f13e7
Aris Dela Cruz <ardelacruz@mssngsolutions.ph>
2017-09-20 19:16:32 +0800

    Renamed class to DecimalToHexConverter.

--- src/main/java/com/ariscdc/coding/problem/math/DecimalToHexConverter.java
+++ src/main/java/com/ariscdc/coding/problem/math/DecimalToHexConverter.java
@@ -5,7 +5,7 @@ package com.ariscdc.coding.problem.math;
  *
  * 20170906 2030-2041 (11 mins.)
  */
-public class Base16NumberConverter {
+public class DecimalToHexConverter {
 
     private static final String[] hexChars =
             { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
@@ -31,7 +31,7 @@ public class Base16NumberConverter {
 
         int decimal = 2321;
 
-        String hex = Base16NumberConverter.decimalToHex(decimal);
+        String hex = DecimalToHexConverter.decimalToHex(decimal);
         System.out.println(hex);
     }
 }
