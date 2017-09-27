f83decb44f294315aff362b3281fe80db04ef2d8
Aris Dela Cruz <ardelacruz@mssngsolutions.ph>
2017-09-27 20:55:21 +0800

    Updated documentation. Discovered problem wherein implementation cannot handle input with duplicate values.

--- src/main/java/com/ariscdc/coding/algorithm/sort/QuickSort.java
+++ src/main/java/com/ariscdc/coding/algorithm/sort/QuickSort.java
@@ -2,6 +2,8 @@ package com.ariscdc.coding.algorithm.sort;
 
 /**
  * @author ariscdc
+ * Aris Dela Cruz
+ * https://github.com/ariscdc
  *
  * 6. Quick Sort
  *
@@ -11,8 +13,9 @@ package com.ariscdc.coding.algorithm.sort;
  * larger than the Pivot on the other.
  * This Pivot partition is applied to all sub-lists till the list is sorted.
  *
- * Time Complexity: O(N log N) on Average case.
- * Space Complexity: O(log N) for the recursion call stack. Worst case could be O(N)
+ * Time Complexity: O(n log n) on Average case.
+ * Space Complexity: O(log n) for the recursion call stack. Worst case could be O(n)
+ *
  * Not Stable, entities which are equal are NOT maintained in original order.
  * Not Adaptive, goes into recursive call and cannot see the big picture if the list is already sorted.
  *
@@ -65,7 +68,7 @@ public class QuickSort {
 
     private static void swapElements(int[] elements, int i, int j, int step) {
 
-        if (i == j) return;
+        if (i == j || elements[i] == elements[j]) return;
         int temp = elements[i];
         elements[i] = elements[j];
         elements[j] = temp;
@@ -74,13 +77,21 @@ public class QuickSort {
 
     public static void main(String[] args) {
 
-//        int[] elements = { 1, 3, 2, 1, 9, 7, 0, 5, 3, 8, 6, 4, 3 };
-//        int[] elements = { 1, 3, 2, 4, 9, 7, 8, 5, 6, 10 };
-//        int[] elements = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-        int[] elements = { 3, 6, 4, 2, 9, 8 };
-//        int[] elements = { 8, 2 };
-
-        print(elements, 0);
-        sort(elements, 0, elements.length - 1, 1);
+        int[][] testCases = {
+                { 1, 2, 3, 4, 5 },
+                { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+                { 8, 2 },
+                { 3, 6, 4, 2, 9, 8 },
+                { 1, 3, 2, 4, 9, 7, 8, 5, 6, 10 }
+                // TODO Fix cannot handle input with duplicate values
+                // { 1, 1 },
+                // { 3, 3, 3, 3, 3 },
+                // { 1, 3, 2, 1, 9, 7, 0, 5, 3, 8, 6, 4, 3 }
+        };
+        for (int[] testCase : testCases) {
+            System.out.println("------------------------------");
+            print(testCase, 0);
+            sort(testCase, 0, testCase.length - 1, 1);
+        }
     }
 }
